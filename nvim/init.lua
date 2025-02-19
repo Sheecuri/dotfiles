cfca257943fe212f9808a139af6965a91d7bbd54
Patrick Mehl <patrick.mehl@campusjaeger.de>
2025-02-19 13:59:22 +0100

    Add vue support

--- nvim/init.lua
+++ nvim/init.lua
@@ -78,7 +78,63 @@ require('lspconfig').intelephense.setup({})
 -- })
 require('lspconfig').lua_ls.setup({})
 require('lspconfig').jsonls.setup({})
-require('lspconfig').ts_ls.setup({})
+require('lspconfig').volar.setup({
+    init_options = {
+        vue = {
+            hybridMode = false,
+        },
+    },
+    settings = {
+        typescript = {
+            inlayHints = {
+                enumMemberValues = {
+                    enabled = true,
+                },
+                functionLikeReturnTypes = {
+                    enabled = true,
+                },
+                propertyDeclarationTypes = {
+                    enabled = true,
+                },
+                parameterTypes = {
+                    enabled = true,
+                    suppressWhenArgumentMatchesName = true,
+                },
+                variableTypes = {
+                    enabled = true,
+                },
+            },
+        },
+    },
+})
+require('lspconfig').ts_ls.setup({
+    init_options = {
+        plugins = {
+            {
+                name = '@vue/typescript-plugin',
+                location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                languages = { 'vue' },
+            },
+        },
+    },
+    settings = {
+        typescript = {
+            tsserver = {
+                useSyntaxServer = false,
+            },
+            inlayHints = {
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+            },
+        },
+    },
+})
 -- require('lspconfig').rust_analyzer.setup({})
 
 require("mason").setup()
